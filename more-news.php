1aee4c3cad5ca193bb16c9d9e1ec6ad9c3d11692
Shubham Maurya <pnssoftwares7@gmail.com>
2019-07-16 04:01:22 +0530

    updated

--- more-news.php
+++ more-news.php
@@ -43,21 +43,21 @@ crossorigin="anonymous">
     <div class="progress-bar" id="myBar"></div>
   </div> 
 
-            
+ 
 <header id="f">
 <div class="logo">
 <ul>
 <li><a href="index.php"><img src="images\imnitian.png"></a></li>
-<li><a href="index.php" style="font-family: 'Dancing Script', cursive;color:white;font-weight:bold;font-size:13px;letter-spacing: 0.7px;">I AM AN NITIAN</a></li>
+<li><a href="index.php" style="font-family: 'Dancing Script', cursive;color:white;font-weight:bold;font-size:13px;letter-spacing: 0.7px;" >I AM AN NITIAN</a></li>
 </ul>
 </div>
 <nav class="active">
 <ul>
-<li><a href="index.php" class="homex" >Home</a></li>
+<li><a href="index.php" class="home" >Home</a></li>
 <li><a href="team.html" class="homex">Our team</a></li>
-<li><a href="more-news.php" class="homex">News</a></li>
-<li><a href="index.php#about" class="homex">About Us</a></li>
-<li><a class="sub-menu" style="width:115px;">Exams <i class="fas fa-caret-square-down"></i></a>
+<li><a href="more-news.php" class="homex" >News</a></li>
+<li><a href="#about" class="homex" id="about_nav">About Us</a></li>
+<li><a class="sub-menu" style="width:115px;" id="exam">Exams <i class="fas fa-caret-square-down"></i></a>
 <ul>
 <li style="width:115px;"><a href="#">Jee</a></li>
 <li style="width:115px;"><a href="#">Gate</a></li>
@@ -67,15 +67,40 @@ crossorigin="anonymous">
 </ul>
 </li>
 <li>
-<button class="homex" onclick="search()" style="text-transform: uppercase;letter-spacing: 0.8px; font-weight: bold;">Search <i class="fas fa-search"></i></button>
+<button onclick="search()" style="text-transform: uppercase;letter-spacing: 0.8px; font-weight: bold;" id="searching">Search <i class="fas fa-search"></i></button>
 </li>
-<li><a class="homex" style="cursor:pointer; width:150px;text-align:center;margin-left:-1rem;">User
-
+<li><a class="homex" style="cursor:pointer; width:150px;text-align:center;margin-left:-1rem;" id="user">
+<?php
+  if(isset($_SESSION['name'])&& !empty($_SESSION['name']))
+  {
+    echo $_SESSION['name'];
+  }
+  else
+  {
+    echo 'User';
+  }
+?>
 &nbsp <i class="fas fa-user-graduate"></i></a>
 <ul style="margin-left:-1rem;">
 <!-- Hide and Show login and php buttons -->
 
+<?php
+if( isset($_SESSION['user_type']) && !empty($_SESSION['user_type']))
+{
+  if($_SESSION['user_type'] == 1)
+  {
+    echo '<li><a href="edit_news.php" class="homex">Admin</a></li>';
+  }
+}
+?>
 
+<?php if( isset($_SESSION['name']) && !empty($_SESSION['name']))
+{
+?>
+<li><a style="cursor:pointer;"  href="logout.php" >Logout</a></li>
+<?php }else{ ?>
+<li><a style="cursor:pointer;"  onclick="show_log()" id="login_nav">Login</a></li>
+<?php } ?>
 
 </ul>
 </li>
@@ -85,13 +110,15 @@ crossorigin="anonymous">
 <div class="menu-toggle">
 <i class="fas fa-bars"></i>
 </div>
+<img src="images/cutk.png" class="cut_nav" style="display:none;">
 
 <!--================ Search Box ====================-->
 <div class="search-popup" id="searchx" style="display:none;">
     <div><input type="text"  placeholder="Search"><button><i class="fas fa-search"></i></button></div>
   </div>
-
 </header>
+
+
 <!--===============   Main Banner   ==================-->
  <div class="banner">
   <div class="overlay">
@@ -147,56 +174,120 @@ crossorigin="anonymous">
              ?>
           
               </div>
-              <div class="nad">
+
+              <div class="nad" style="display:none;">
   <img src="images/dynamo.gif"  class="nad-img">
   <img src="images/ad-demo.jpg"  class="nad-img">
               </div>
 
               <footer>
-              <div class="datay">
-                <div><p class="y">Colleges</p>
-                  <div class="linex"></div>
-                <p><a href="#">IIT</a></p>
-                <p><a href="#">NIT</a></p>
-                <p><a href="#">IIIT</a></p>
-                <p><a href="#">AIIMS</a></p>
-                </div>
-                <div>
-                    <p class="y">Exams</p>
-                    <div class="linex"></div>
-                      <p><a href="#">Jee Advance</a></p>
-                      <p><a href="#">Jee Mains</a></p>
-                      <p><a href="#">Gate</a></p>
-                      <p><a href="#">Neet</a></p>  
-                      <p><a href="#">Gre</a></p>  
-                    </div>
-                <div><p class="y">Cutoff</p> 
-                  <div class="linex"></div>
-                  <p><a href="#">IITs </a></p>
-                  <p><a href="#">NITs</a></p>
-                  <p><a href="#">IIITs</a></p>
-                  <p><a href="#">AIIMS</a></p></div>
-                <div><p class="y">Links</p>
-                  <div class="linex"></div>
-                  <p><a href="#">Disclaimer</a></p>
-                  <p><a href="#">College Reviews</a></p>
-                  <p><a href="#">College Ranking</a></p>
-                  <p><a href="#">About Us</a></p>
-                  <p><a href="#">News</a></p></div>
-              </div>
-            <div class="datad">
-            <p class="x">Feel Free To Contact Us</p>
-            <p>iamannitian@gmail.com &nbsp &nbsp| &nbsp +91-9055667606 &nbsp | &nbsp  +91-9055667606</p>
-            </div>
-          <div class="container">
-          <p class="copyright">COPYRIGHT&nbsp<i class="far fa-copyright"></i>
-          2019 &nbsp| &nbsp I AM AN NITIAN &nbsp | &nbsp DESIGNED AND DEVELOPED BY SHUBHAM MAURYA &nbsp|&nbsp NIT SRINAGAR</p>
+    <div class="datay">
+              
+      <div><p class="y">Colleges</p>
+        <div class="linex" id="lx"></div>
+      <p><a href="#">IIT</a></p>
+      <p><a href="#">NIT</a></p>
+      <p><a href="#">IIIT</a></p>
+      <p><a href="#">AIIMS</a></p>
+      </div>
+      <div>
+          <p class="y">Exams</p>
+          <div class="linex" id="ly"></div>
+            <p><a href="#">Jee Advance</a></p>
+            <p><a href="#">Jee Mains</a></p>
+            <p><a href="#">Gate</a></p>
+            <p><a href="#">Neet</a></p>  
+            <p><a href="#">Gre</a></p>  
           </div>
-          </footer>
+
+
+      <div><p class="y">Cutoff</p>
+        <div class="linex" id="lz"></div>
+        <p><a href="#">IITs </a></p>
+        <p><a href="#">NITs</a></p>
+        <p><a href="#">IIITs</a></p>
+        <p><a href="#">AIIMS</a></p></div>
+
+      <div><p class="y">Links</p>
+        <div class="linex" id="la"></div>
+        <p><a href="#">Disclaimer</a></p>
+        <p><a href="#">College Reviews</a></p>
+        <p><a href="#">College Ranking</a></p>
+        <p><a href="#">About Us</a></p>
+        <p><a href="#">News</a></p></div>
+        
+    </div>
+
+
+  <div class="datad">
+  <p class="x">Feel Free To Contact Us</p>
+  <p id="cont">iamannitian@gmail.com &nbsp &nbsp| &nbsp +91-9055667606 &nbsp | &nbsp  +91-9055667606</p>
+ <span id="respo_contact">
+ <P>iamannitian@gmail.com</p> 
+ <P>+91-9055667606</p> 
+ <P>+91-9055667606</p> 
+   </span>
+</div>
+<!--============== Bottom Container =====================-->
+ <div class="container">
+<p class="copyright">COPYRIGHT&nbsp<i class="far fa-copyright"></i>
+2019 &nbsp| &nbsp I AM AN NITIAN <span id="developer">&nbsp | &nbsp DESIGNED AND DEVELOPED BY SHUBHAM MAURYA &nbsp|&nbsp NIT SRINAGAR</span></p>
+<p id="and_copy" class="copyright">Developed by Shubham Maurya</p>
+</div>
+</footer>
 
     </body>
 </html>
-<!--===============  Microsoft's JQuery CDN =================-->
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.js" type="text/javascript"></script>
-<!--==================  Local Js Files  ==================-->
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+<!--==================  TweenMax CDN  ==================-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
+<!--==================  SweetAlert2 CDN  ==================-->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8" type="text/javascript"></script>
 <script src="js/main.js" type="text/javascript"></script>
+
+<script>
+
+
+ /*==================== Menu toggle =========================*/
+
+ $(document).ready(function(){
+
+if (window.matchMedia('(max-width:721px)').matches)
+{
+  
+  $('.cut_nav').click(function(){
+    $(this).css('display','none');
+    $('.menu-toggle').css('display','block');
+    TweenMax.to('.active',0.5,{scaleX: 0});
+  })
+
+  $('.menu-toggle').click(function(){
+    $(this).css('display','none');
+    $('.cut_nav').css('display','block');
+    TweenMax.to('.active',0.5,{scaleX: 1});
+  })
+
+
+$('#login_nav').click(function()
+{
+  TweenMax.to('.active',0.1,{scaleX: 0});  
+  $('.cut_nav').css('display','none');
+  $('.menu-toggle').css('display','block');
+})
+
+
+$('#about_nav').click(function()
+{
+  TweenMax.to('.active',0.1,{scaleX: 0});  
+  $('.cut_nav').css('display','none');
+  $('.menu-toggle').css('display','block');
+})
+
+
+}
+})
+
+</script>
+
+
+
