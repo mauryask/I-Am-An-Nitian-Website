3641bcedc0d6a1d9a89b203ebc325574f676f75b
Shubham Maurya <pnssoftwares7@gmail.com>
2019-07-23 20:28:57 +0530

    updated

--- ranking-16.php
+++ ranking-16.php
@@ -30,6 +30,7 @@ crossorigin="anonymous">
 <link href="css/back-to-top.css" type="text/css" rel="stylesheet">
 <link href="css/footer.css" type="text/css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
+<link href="css/login_register.css" type="text/css" rel="stylesheet">
 <style>
 
 </style>
@@ -50,19 +51,19 @@ crossorigin="anonymous">
     <div class="progress-bar" id="myBar"></div>
   </div>   
 
-<header id="f" style="background:black;">
-<div class="logo">
+  <header id="f" style="background:black;">
+<div class="logo" >
 <ul>
 <li><a href="index.php"><img src="images\imnitian.png"></a></li>
-<li><a href="index.php" style="font-family: 'Dancing Script', cursive;color:white;font-weight:bold;font-size:13px;letter-spacing: 0.7px;" >I AM AN NITIAN</a></li>
+<li class="logo_txt"><a href="index.php" style="font-family: 'Dancing Script', cursive;color:white;font-weight:bold;font-size:13px;letter-spacing: 0.7px;" >I AM AN NITIAN</a></li>
 </ul>
 </div>
 <nav class="active">
 <ul>
-<li><a href="index.php" class="home" >Home</a></li>
+<li><a href="index.php" class="homex" >Home</a></li>
 <li><a href="team.html" class="homex">Our team</a></li>
 <li><a href="more-news.php" class="homex" >News</a></li>
-<li><a href="#about" class="homex" id="about_nav">About Us</a></li>
+<li><a href="index.php#about" class="homex" id="about_nav">About Us</a></li>
 <li><a class="sub-menu" style="width:115px;" id="exam">Exams <i class="fas fa-caret-square-down"></i></a>
 <ul>
 <li style="width:115px;"><a href="#">Jee</a></li>
@@ -91,6 +92,7 @@ crossorigin="anonymous">
 <!-- Hide and Show login and php buttons -->
 
 <?php
+
 if( isset($_SESSION['user_type']) && !empty($_SESSION['user_type']))
 {
   if($_SESSION['user_type'] == 1)
@@ -98,14 +100,17 @@ if( isset($_SESSION['user_type']) && !empty($_SESSION['user_type']))
     echo '<li><a href="edit_news.php" class="homex">Admin</a></li>';
   }
 }
+
 ?>
 
 <?php if( isset($_SESSION['name']) && !empty($_SESSION['name']))
 {
+
 ?>
 <li><a style="cursor:pointer;"  href="logout.php" >Logout</a></li>
 <?php }else{ ?>
 <li><a style="cursor:pointer;"  onclick="show_log()" id="login_nav">Login</a></li>
+<li><a style="cursor:pointer;"  onclick="show_signup()" id="register_nav">Register</a></li>
 <?php } ?>
 
 </ul>
@@ -123,23 +128,16 @@ if( isset($_SESSION['user_type']) && !empty($_SESSION['user_type']))
     <div><input type="text"  placeholder="Search"><button><i class="fas fa-search"></i></button></div>
   </div>
 
-<style>
-  @media (max-width:991px){
-
-  }
-  </style>
-
-</header>
+</header>          
+ 
 
 
-
-
-  
 <div class="center">
 <p>NITs Ranking <span id="rank_yr" ><span class="break"><br></span>2016</span></p>
 <p class="sub-center">rankings are based <span class="brk"><br></span>on nirf survey</p>
 <div class="line_rnk"> </div>
 </div>
+
 
 
 <div class="clg">
@@ -170,8 +168,8 @@ if( isset($_SESSION['user_type']) && !empty($_SESSION['user_type']))
 <table>
 <th>Serial No.</th>
 <th>NIT Name</th>
-<th>Overall Ranking  2016</th>
-<th>NIT Ranking 2016</th>
+<th>Overall Ranking <span id="change_rk"> 2016</span></th>
+<th>NIT Ranking <span  id="change_rs">2016</span></th>
 <tbody id="rank_16">
 
 </tbody>
@@ -180,14 +178,96 @@ if( isset($_SESSION['user_type']) && !empty($_SESSION['user_type']))
 </table>
 
 
+
 <div class="linkx">
 <p class="links"> Explore More</p>
 <div class="line"></div>
-<p><a href="ranking-19.php" class="x" >NITs Ranking 2019</a></p>
-<p><a href="ranking-18.php" class="x" >NITs Ranking 2018</a></p>
-<p><a href="ranking-17.php" class="x" >NITs Ranking 2017</a></p>
+<p><a href="ranking-19.php" class="x" id="click_19">NITs Ranking 2019</a></p>
+<p><a href="ranking-18.php" class="x" id="click_18">NITs Ranking 2018</a></p>
+<p><a href="ranking-17.php" class="x" id="click_17">NITs Ranking 2017</a></p>
 </div>
 </div>
+
+
+
+
+
+<!--============== Signup Popup ==================-->
+<div class="signup" id="sign">
+<div class="sign-card">
+ <div class="sign-img">
+  <div class="links">
+    <p class="p1">Quick Links</p>
+    <div class="msgx" ><p id="msgx" ></p></div>
+    <ul>
+     <a href="#"> <li>Colleges</li></a>
+     <a href="#"> <li>Exams</li></a>
+     <a href="#"> <li>Cutoff</li></a>
+     <a href="#"> <li>News</li></a>
+     <a href="#"> <li>Events</li></a>
+     <a href="#"> <li>Important Dates</li></a>
+      </ul>
+    <p class="p2"><a href="#">privacy policy</a></p>
+  </div>
+ </div>
+ <div class="sign-form">
+   <img class="cancel" onclick="cancel()" src="images/cut.png">
+    <p class="mainh">Register Here</p>
+    <div class="msgx"  id="msgk"><p id="msgy"></p></div>
+   <div>
+     <form method="post" autocomplete="off">
+   <input autocomplete="off" type="text"  name="name"  id="name" placeholder="Name"   onmousedown="this.style.paddingLeft='10px';this.style.transition='0.2s'" onmouseout="this.style.paddingLeft='2px';this.style.transition='0.2s'">
+   <input   autocomplete="off"  type="text" name="user_email" id="user_email"  placeholder="Email"  onmousedown="this.style.paddingLeft='10px';this.style.transition='0.2s'" onmouseout="this.style.paddingLeft='2px';this.style.transition='0.2s'">
+   <input  type="text"  name="phone" id="phone" placeholder="Mobile Number"  onmousedown="this.style.paddingLeft='10px';this.style.transition='0.2s'" onmouseout="this.style.paddingLeft='2px';this.style.transition='0.2s'">
+   <select name="clg" id="clg"  onmousedown="this.style.paddingLeft='10px';this.style.transition='0.2s'" onmouseout="this.style.paddingLeft='2px';this.style.transition='0.2s'">
+      <option>Select College</option>
+      <option>National Institute Of technology Srinagar</option> 
+      <option>National Institute Of technology Srinagar Silchar</option>
+      <option>National Institute Of technology Srinagar Trichy</option>
+      <option>Motilal Nehru Institute Of Technology Allahabad</option>
+      <option>Malviya Natioanl Institute Of Technology Jaipur</option>
+      <option>Other</option>
+      </select>
+      <select name="state" id="state"  onmousedown="this.style.paddingLeft='10px';this.style.transition='0.2s'" onmouseout="this.style.paddingLeft='2px';this.style.transition='0.2s'">
+          <option>Select Your State</option>
+          <option>Uttar Pradesh</option>
+          <option>Jammu and Kashmir</option>
+          <option>Rajsthan</option>
+          <option>Tamilnadu</option>
+          <option>Uttrakhand</option>
+          <option>Bihar</option>
+          </select>
+          <input  autocomplete="off" name="user_pass" id="user_pass" type="password" placeholder="Password"  onmousedown="this.style.paddingLeft='10px';this.style.transition='0.2s'" onmouseout="this.style.paddingLeft='2px';this.style.transition='0.2s'">
+          <input autocomplete="off" type="password"  id="cnf_pass" name="cnf_pass" placeholder="Confirm Password" onmousedown="this.style.paddingLeft='10px';this.style.transition='0.2s'" onmouseout="this.style.paddingLeft='2px';this.style.transition='0.2s'">
+          <button name="register" id="register" type="button"   class="register">Register</button>
+        </form>
+         <p> <a onclick="show_log();cancel();">already a member? login</a></p>
+  </div>
+ </div>
+</div>
+</div>
+<!--=============== Login Popup =================-->
+<div class="login" id="log" >
+<div class="log-card">
+    <img class="cancel-log"  src="images/cut.png" onclick="cancel_log()">
+<div>
+<div class="log-msg"  ><p id="message"><p></div>
+  <form  method="post" autocomplete="off">
+  <input  id="email" type="text" autocomplete="off" placeholder="Email"  onmousedown="this.style.paddingLeft='10px';this.style.transition='0.2s'" onmouseout="this.style.paddingLeft='2px';this.style.transition='0.2s'">
+  <input autocomplete="off"  id="pass" type="password" placeholder="Password"   onmousedown="this.style.paddingLeft='10px';this.style.transition='0.2s'" onmouseout="this.style.paddingLeft='2px';this.style.transition='0.2s'">
+<div class="select" id="select">
+  <input type="radio" name="user" id="user" value="1" checked>&nbsp <label class="lab1">User</label>
+  <input type="radio" name="user" id="admin" value="2">&nbsp <label>Admin</label>
+ </div>
+  <button type="button"  name="login" id="login">Login</button>
+</form>
+  <p class="signx"><a onclick="show_signup();cancel_log();">new member? register</a></p>
+</div>
+</div>
+</div>
+
+
+
 
 
  <!--============== Footer Section ==================-->
@@ -250,28 +330,29 @@ if( isset($_SESSION['user_type']) && !empty($_SESSION['user_type']))
 </body>
 </html>
 
-<!--===============  Microsoft's JQuery CDN =================-->
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.js" type="text/javascript"></script>
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
 <!--==================  TweenMax CDN  ==================-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
+<!--==================  SweetAlert2 CDN  ==================-->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8" type="text/javascript"></script>
+<!--==================  SweetAlert CDN  ==================-->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script   type="text/javascript" src="js/ajax-register.js"></script>
+<script   type="text/javascript" src="js/ajax-login.js"></script>
+<script src="js/main.js" type="text/javascript"></script>
 
-<script   type="text/javascript" src="js/main.js"></script>
 
 <script>
 var id;
 $(document).ready(function(){
-  var id;
-$(document).ready(function(){
- id= 1;
+ id= 4;
   $('#rank_16').load('get_ranking.php', {id : id});
 });
 
 
-});
 
-
-/*==================== Search ranking =========================*/
-$(function()
+ /*==================== Search ranking =========================*/
+ $(function()
 { //instead of $(document).ready(function(){}) you ncan use this short hand property
     $('#search').on('keyup',function(){
 
@@ -283,41 +364,86 @@ $(function()
 })
 
 
- /*==================== Menu toggle =========================*/
+/*==================== Menu toggle =========================*/
 
- $(document).ready(function(){
+$(document).ready(function(){
 
 if (window.matchMedia('(max-width:721px)').matches)
 {
-  
-  $('.cut_nav').click(function(){
-    $(this).css('display','none');
-    $('.menu-toggle').css('display','block');
-    TweenMax.to('.active',0.5,{scaleX: 0});
-  })
 
-  $('.menu-toggle').click(function(){
-    $(this).css('display','none');
-    $('.cut_nav').css('display','block');
-    TweenMax.to('.active',0.5,{scaleX: 1});
-  })
-
-
-$('#login_nav').click(function()
-{
-  TweenMax.to('.active',0.1,{scaleX: 0});  
-  $('.cut_nav').css('display','none');
+$('.cut_nav').click(function(){
+  $(this).css('display','none');
   $('.menu-toggle').css('display','block');
+  TweenMax.to('.active',0.5,{scaleX: 0});
+  $('nav').css('z-index', '1');
+  $('.logo').css('marginLeft', '0px');
+  $('.logo').css('marginTop', '0px');
+  $('.logo_txt').css('marginTop', '0px');
+
+  setTimeout(function(){
+  
+    $('header').css('background', 'black');
+  
+  },200)
+
 })
 
+$(window).scroll(function(){
+$('header').css('background', 'black');
+})
+
+
+
+$('.menu-toggle').click(function(){
+  $(this).css('display','none');
+  $('.cut_nav').css('display','block');
+
+  $('nav').css('z-index', '4');
+
+  $('header').css('background', 'transparent');
+  $('.cut_nav').css('background','black');
+  TweenMax.to('.active',0.4,{scaleX: 1});
+
+  setTimeout(function(){
+    $('.logo').css('marginLeft', '62px');
+    $('.logo').css('marginTop', '18px');
+    $('.logo_txt').css('marginTop', '10px');
+  },200);
+
+  setTimeout(function(){
+    $('nav').css('z-index', '-1');
+  },350);
+ 
+})
+$('#login_nav').click(function()
+{
+$('.logo').css('marginLeft', '0px');
+$('.logo').css('marginTop', '0px');
+TweenMax.to('.active',0.1,{scaleX: 0});  
+$('.cut_nav').css('display','none');
+$('.menu-toggle').css('display','block');
+$('header').css('background', 'black');
+$('.logo_txt').css('marginTop', '0px');
+})
+
+$('#register_nav').click(function(){
+$('.logo').css('marginLeft', '0px');
+$('.logo').css('marginTop', '0px');
+TweenMax.to('.active',0.1,{scaleX: 0});  
+$('.cut_nav').css('display','none');
+$('.menu-toggle').css('display','block');
+$('header').css('background', 'black');
+$('.logo_txt').css('marginTop', '0px');
+})
 
 $('#about_nav').click(function()
 {
-  TweenMax.to('.active',0.1,{scaleX: 0});  
-  $('.cut_nav').css('display','none');
-  $('.menu-toggle').css('display','block');
+$('.logo').css('marginLeft', '0px');
+$('.logo').css('marginTop', '0px');
+TweenMax.to('.active',0.1,{scaleX: 0});  
+$('.cut_nav').css('display','none');
+$('.menu-toggle').css('display','block');
 })
-
 
 }
 })
